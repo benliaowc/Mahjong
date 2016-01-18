5b6fdc78b78bfb012537c082db2171bf188623a6
hyww13 <willy2202@gmail.com>
2016-01-18 13:33:48 +0800

    Added Player.class

---
+++ Player.java
@@ -0,0 +1,34 @@
+import java.lang.*;
+import java.util.*;
+
+public abstract class Player{
+	private String name;
+	public Player(String s, int i){
+		name = s;
+	}
+
+	@Override
+	public String toString(){
+		return "Hi, I am Player "+name+".";
+	}
+
+	//the 13 tiles at the beginning
+	public abstract void initTiles(ArrayList<Tile> t);
+
+	//ask the player whether to draw/chow/pong/kong/hu or not
+	public abstract boolean doDraw(Tile t, ArrayList<ArrayList<Tile>> currentTable);
+	public abstract boolean doChow(Tile t, ArrayList<ArrayList<Tile>> currentTable);
+	public abstract boolean doPong(Tile t, ArrayList<ArrayList<Tile>> currentTable);
+	public abstract boolean doKong(Tile t, ArrayList<ArrayList<Tile>> currentTable);
+	public abstract boolean doHu(Tile t, ArrayList<ArrayList<Tile>> currentTable);
+
+	//if chow/pong failed, use this method to notify player
+	public abstract void failed();
+
+	//else give the player new tile, and ask the player which tile to discard. return null if the player wants to Kong
+	public abstract Tile replace(Tile tile, ArrayList<ArrayList<Tile>> currentTable);
+
+	//ask the player which to kong, remember to call replace() after kong
+	public abstract Tile kong(ArrayList<ArrayList<Tile>> currentTable);
+
+}
